f7ac3f7f02592aa1ba53410b90a0423bec790d18
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-01-21 15:29:21 +0100

    Updated LICENSE header

--- networkmanager.cpp
+++ networkmanager.cpp
@@ -1,18 +1,18 @@
 /*
- *   This file is part of QRail.
+ *   This file is part of Qt-SSE-Demo.
  *
- *   QRail is free software: you can redistribute it and/or modify
+ *   Qt-SSE-Demo is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   QRail is distributed in the hope that it will be useful,
+ *   Qt-SSE-Demo is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
  *   You should have received a copy of the GNU General Public License
- *   along with QRail.  If not, see <http://www.gnu.org/licenses/>.
+ *   along with Qt-SSE-Demo.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "networkmanager.h"
 Network::Manager *Network::Manager::m_instance = nullptr;
